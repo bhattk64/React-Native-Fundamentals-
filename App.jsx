1b276085ecda1b26507780d392b38f607c33c188
kul chandra bhatt <114309808+bhattk64@users.noreply.github.com>
2025-01-17 23:34:14 +0530

    tab navigation in react native

--- App.jsx
+++ App.jsx
@@ -1,12 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import React from 'react';
 import Home from './src/screens/Home';
 import Profile from './src/screens/Profile';
 import Search from './src/screens/Search';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const StackNavigator = () => {
   return (
@@ -17,11 +20,27 @@ const StackNavigator = () => {
     </Stack.Navigator>
   );
 };
+const TabNavigator = () => {
+  return(
+    <Tab.Navigator options={{tabBarActiveTintColor:"red"}}>
+      <Tab.Screen  name="Home" component={Home} options={{tabBarLabel:"Home" ,
+      tabBarIcon:()=>(
+        <Icon name="home" size={30} color="#900" />
+      )
+
+
+      }} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+     {/* <StackNavigator /> */}
+     <TabNavigator />
     </NavigationContainer>
   );
 };
